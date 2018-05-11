93698a2f9f106bdf1eb31efa31a7c5e3c8e6ba43
rrafaelpinto <rrafaelpinto@gmail.com>
2018-05-11 12:58:31 -0300

    substituindo %s por %s

--- treetagger.py
+++ treetagger.py
@@ -122,11 +122,11 @@ class TreeTagger(TaggerI):
                 searchpath=treetagger_paths,
                 url=_treetagger_url,
                 verbose=verbose)
-        print u'### %s' % self._treetagger_bin
+        print u'### {}'.format(self._treetagger_bin)
 
         if encoding in _treetagger_charset:
             self._encoding = encoding
-        print u'#### %s' % self._encoding
+        print u'#### {}'.format(self._encoding)
         
     def tag(self, sentences):
         """Tags a single sentence: a list of words.
